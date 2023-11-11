224081770369366bdedb7e6a487f0bb5d24b96de
TianoRao <TianoRao>
2023-11-11 23:17:16 +0000

    Update

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+"""
+Main cli or app entry point
+"""
+
 from mylib.extract import extract
 from mylib.transform_load import load
-# from mylib.query_viz import query_transform, viz
-import os
+from mylib.query_viz import query_transform, viz
+import os 
 
 
 if __name__ == "__main__":
@@ -9,5 +13,5 @@ if __name__ == "__main__":
     print(current_directory)
     extract()
     load()
-    # query_transform()
-    # viz()
+    query_transform()
+    viz()
